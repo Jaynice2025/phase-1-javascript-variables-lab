ade9740da5c0d474e7041fc2583e6074ba2c1ed7
Jaynice <janyce.wathuo@student.moringaschool.com>
2025-03-13 11:49:19 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+
+const companyName = "Scuber"; // ✅ Use const
+
+let mostProfitableNeighborhood = "Chelsea"; // ✅ Use let
+
+let companyCeo = "Susan Smith"; // ✅ Use let
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
